b08c1a1c31642c095f3c91f785d03ae3a8b4c4a4
earthromal <m.romal0015@gmail.com>
2024-11-18 08:14:36 +0600

    config

--- config/firebase.init.js
+++ config/firebase.init.js
@@ -7,12 +7,12 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDG7R8WlaKk8_Q8x2oZUtvOpAxPdMJ1ZXk',
-  authDomain: 'earthco-ecad3.firebaseapp.com',
-  projectId: 'earthco-ecad3',
-  storageBucket: 'earthco-ecad3.appspot.com',
-  messagingSenderId: '1031853299242',
-  appId: '1:1031853299242:web:d7dc3bd030b07a362c5553',
+  apiKey: "AIzaSyDgCq_FrQ5uMf8tvbJB6bP2YuxOpN2TLbY",
+  authDomain: "madrassa-3d986.firebaseapp.com",
+  projectId: "madrassa-3d986",
+  storageBucket: "madrassa-3d986.appspot.com",
+  messagingSenderId: "357816431577",
+  appId: "1:357816431577:web:574cafa5a8f2e91cc51fc8"
 }
 
 // Initialize Firebase
